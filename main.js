c0b4a56a389eb4138e17d7c650f01bc8824fbacb
Thiago Poiani <thpoiani@gmail.com>
2015-09-24 22:00:10 -0300

    Leaflet

---
+++ main.js
@@ -0,0 +1,17 @@
+navigator.geolocation.getCurrentPosition(function(position){
+    var latitude = position.coords.latitude
+    var longitude = position.coords.longitude
+    alert([latitude, longitude])
+
+    var map = L.map('map').setView([latitude, longitude], 15);
+
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; Contributors',
+        maxZoom: 18,
+    }).addTo(map);
+
+    L.marker([latitude, longitude])
+        .addTo(map)
+        .bindPopup("Hello world!")
+        .openPopup();
+});
